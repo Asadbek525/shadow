7ce46d76eeb3876913176eb1f5c06373157a1794
Asadbek <qarshiyevasadbekahmadovich@gmail.com>
2021-03-17 12:15:39 +0500

    Reset button added,background colors changed

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
 let output_code = document.getElementById('output--code');
-    let output_code1 = document.getElementById('output--code1');
-    let box = document.getElementById('box');
-    let text = document.getElementById('text');
-    const onChange = (event) => {
-      return event.target.value;
-    };
-    const onKeyup = () => {
-      let style = onChange(event);
-      box.style = style;
-      console.log('working');
-      output_code.innerText = style;
-    };
-    const onFocus = () => {
-      onKeyup();
-    };
-    const onChangeText = (event) => {
-      return event.target.value;
-    };
-    const onKeyupText = () => {
-      let style = onChangeText(event);
-      text.style = style;
-      console.log('working');
-      output_code1.innerText = style;
-    };
-    const onFocusText = () => {
-      onKeyupText();
-    };
+let output_code1 = document.getElementById('output--code1');
+let box = document.getElementById('box');
+let text = document.getElementById('text');
+const onChange = (event) => {
+  return event.target.value;
+};
+const onKeyup = () => {
+  let style = onChange(event);
+  box.style = style;
+  console.log('working');
+  output_code.innerText = style;
+};
+const onFocus = () => {
+  onKeyup();
+};
+const onChangeText = (event) => {
+  return event.target.value;
+};
+const onKeyupText = () => {
+  let style = onChangeText(event);
+  text.style = style;
+  console.log('working');
+  output_code1.innerText = style;
+};
+const onFocusText = () => {
+  onKeyupText();
+};
+const reset = () => {
+  text.style = '';
+  box.style = '';
+  output_code1.innerText = null;
+  output_code.innerText = null;
+};
